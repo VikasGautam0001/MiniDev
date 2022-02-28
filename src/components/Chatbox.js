1b9e7b006caf402b60cef1eec3032dcbd0b97c15
VikasGautam0001 <vg5823967@gmail.com>
2022-02-28 12:33:04 +0530

    chatbox.css added

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,10 +1,10 @@
-
+import "./css/Chatbox.css"
 const Chatbox = () => {
   return (
     <div>
         <h3>Chatbox</h3>
         <form>
-            <input type="text" placeholder="Enter a message" ></input>
+            <input type="text" placeholder="Enter a message" className="message" ></input>
             <input type="submit" name="submit" value="send"></input>
         </form>
     </div>
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,8 +6,8 @@ const Editor = () => {
         <form>
             <h3>Code Editor</h3>
             <textarea className="textArea" name="codeEditor"> </textarea>
-            <input type="button" value="Run" style={{width:'220px'}}></input>
-            <input type="button" value="Submit" style={{width:'220px'}}></input>
+            <input type="button" value="Run" className="run" ></input>
+            <input type="button" value="Submit" className="submit"></input>
         </form>
     </div>
   )
