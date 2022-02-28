203941f29fd8259bddb5cfa6c3c20df57183f9e5
VikasGautam0001 <vg5823967@gmail.com>
2022-02-28 10:28:06 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+function App() {
+  return (
+    <section class="container">
+      <div class="row">
+        <div class="col-sm-4 question">
+          <h3>Question</h3>
+          <p>Lorem ipsum dolor..</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>IDE</h3>
+          <p>Lorem ipsum dolor..</p>
+        </div>
+        <div class="col-sm-4">
+          <h3>Chat Box</h3>
+          <p>Lorem ipsum dolor..</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
