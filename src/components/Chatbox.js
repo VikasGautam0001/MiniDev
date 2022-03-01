2823385e2f0d1766d35f0daaad3e0470729f386b
VikasGautam0001 <vg5823967@gmail.com>
2022-03-01 12:49:35 +0530

    Chatbox messages

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,12 +1,53 @@
+
+import {useState,useEffect} from 'react'
 import "./css/Chatbox.css"
+import ChatboxMessage from "./ChatboxMessage"
+
 const Chatbox = () => {
+
+  const [Messages, setMessages] = useState([])
+  const [Message, setMessage] = useState("")
+  
+
+  const onAdd = (Message) => {
+    setMessages([...Messages , Message])
+  }
+  
+  const onSubmit = (e) => {
+      e.preventDefault()
+
+      if(!Message){
+        alert('Please add text')
+        return
+      }
+
+      onAdd(Message)
+
+      
+      // setMessage('')
+  }
+
   return (
+    
+
     <div>
         <h3 className="header">Chatbox</h3>
-        <form>
-            <input type="text" placeholder="Enter a message" className="message" ></input>
-            <input type="submit" name="submit" value="send"></input>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Enter a message"
+             value={Message} className="message"
+             onChange={(e) => setMessage(e.target.value)}
+            />
+
+            <input type="submit" name="submit" 
+              value='Send'
+            />
         </form>
+
+        {Messages.map((msg) =>(
+            <ChatboxMessage brand={msg} />
+        ))}
+        
+        
     </div>
   )
 }
---
+++ src/components/ChatboxMessage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatboxMessage = (prop) => {
+  return (
+    <div>{prop.brand}</div>
+  )
+}
+
+export default ChatboxMessage
